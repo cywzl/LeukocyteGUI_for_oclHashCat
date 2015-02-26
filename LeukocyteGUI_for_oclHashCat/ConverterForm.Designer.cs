adfc0eba662179e6511cf9b41c4801234bac5329
Igor Kotlerman <igor.kotlerman@gmail.com>
2015-02-26 21:56:13 +0200

    Added readme and license files

--- LeukocyteGUI_for_oclHashCat/ConverterForm.Designer.cs
+++ LeukocyteGUI_for_oclHashCat/ConverterForm.Designer.cs
@@ -1,4 +1,25 @@
-﻿namespace LeukocyteGUI_for_oclHashCat
+﻿//  Copyright (C) 2015  Igor Kotlerman
+
+//  This file is part of LeukocyteGUI_for_oclHashCat.
+
+//  LeukocyteGUI_for_oclHashCat is free software: you can
+//  redistribute it and/or modify it under the terms of the
+//  GNU General Public License as published by the
+//  Free Software Foundation, either version 3
+//  of the License, or (at your option) any later version.
+
+//  LeukocyteGUI_for_oclHashCat is distributed in the hope
+//  that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  
+//  See the GNU General Public License for more details.
+
+//  You should have received a copy of the
+//  GNU General Public License along with
+//  LeukocyteGUI_for_oclHashCat. 
+//  If not, see <http://www.gnu.org/licenses/>.
+
+namespace LeukocyteGUI_for_oclHashCat
 {
     partial class ConverterForm
     {
