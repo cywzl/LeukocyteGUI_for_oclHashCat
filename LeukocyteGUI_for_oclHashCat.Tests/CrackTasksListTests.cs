83e96429b697798a341fd3a66e1ec88a116f0621
Igor Kotlerman <igor.kotlerman@gmail.com>
2016-02-11 17:00:03 +0200

    Added task movement test for CrackTasksList
    - Added test that checks whether CrackTasksList correctly processes task
    movement and calls CrackTaskMoved events with correct EventArgs

--- LeukocyteGUI_for_oclHashCat.Tests/CrackTasksListTests.cs
+++ LeukocyteGUI_for_oclHashCat.Tests/CrackTasksListTests.cs
@@ -8,6 +8,10 @@ namespace LeukocyteGUI_for_oclHashCat.Tests
         bool Add_Single_AddEventCalled_Flag;
         bool Add_Multiple_AddEventCalledAndRightArgs_Flag1;
         bool Add_Multiple_AddEventCalledAndRightArgs_Flag2;
+        bool MoveTask_From2To6_2to5_Flag;
+        bool MoveTask_From2To6_3to2_Flag;
+        bool MoveTask_From2To6_4to3_Flag;
+        bool MoveTask_From2To6_5to4_Flag;
 
         [TestMethod]
         public void Add_Single_AddEventCalled()
@@ -31,7 +35,7 @@ namespace LeukocyteGUI_for_oclHashCat.Tests
             crackTask1.CrackTarget = "test1";
             var crackTask2 = new CrackTask();
             crackTask2.CrackTarget = "test2";
-            
+
             crackTasksList.Add(crackTask1);
             crackTasksList.Add(crackTask2);
 
@@ -39,9 +43,54 @@ namespace LeukocyteGUI_for_oclHashCat.Tests
             Assert.IsTrue(Add_Multiple_AddEventCalledAndRightArgs_Flag2);
         }
 
+        [TestMethod]
+        public void MoveTask_From2To6_Moved_2to5_3to2_4to3_5to4()
+        {
+            var crackTasksList = new CrackTasksList();
+            crackTasksList.CrackTaskMoved += MoveTask_From2To6_CrackTasksList_CrackTaskMoved;
+
+            for (int i = 0; i < 10; i++)
+            {
+                var crackTask = new CrackTask();
+                crackTask.SessionSettings.Session = "task#" + i;
+                crackTasksList.Add(crackTask);
+            }
+
+            crackTasksList.MoveTask(2, 6);
+
+            Assert.IsTrue(MoveTask_From2To6_2to5_Flag);
+            Assert.IsTrue(MoveTask_From2To6_3to2_Flag);
+            Assert.IsTrue(MoveTask_From2To6_4to3_Flag);
+            Assert.IsTrue(MoveTask_From2To6_5to4_Flag);
+        }
+
+        private void MoveTask_From2To6_CrackTasksList_CrackTaskMoved(object sender, CrackTasksListTaskMovedEventArgs e)
+        {
+            if ((e.CrackTaskOldId == 2) && (e.CrackTaskNewId == 5)
+                && (e.CrackTask.SessionSettings.Session == "task#2"))
+            {
+                MoveTask_From2To6_2to5_Flag = true;
+            }
+            if ((e.CrackTaskOldId == 3) && (e.CrackTaskNewId == 2)
+                && (e.CrackTask.SessionSettings.Session == "task#3"))
+            {
+                MoveTask_From2To6_3to2_Flag = true;
+            }
+            if ((e.CrackTaskOldId == 4) && (e.CrackTaskNewId == 3)
+                && (e.CrackTask.SessionSettings.Session == "task#4"))
+            {
+                MoveTask_From2To6_4to3_Flag = true;
+            }
+            if ((e.CrackTaskOldId == 5) && (e.CrackTaskNewId == 4)
+                && (e.CrackTask.SessionSettings.Session == "task#5"))
+            {
+                MoveTask_From2To6_5to4_Flag = true;
+            }
+        }
+
         private void Add_Multiple_CrackTasksList_CrackTaskAdded(object sender, CrackTasksListTaskChangedEventArgs e)
         {
-            if((!Add_Multiple_AddEventCalledAndRightArgs_Flag2)
+            if ((!Add_Multiple_AddEventCalledAndRightArgs_Flag2)
                 && e.CrackTask.CrackTarget == "test1"
                 && e.CrackTaskId == 0)
             {
@@ -59,5 +108,7 @@ namespace LeukocyteGUI_for_oclHashCat.Tests
         {
             Add_Single_AddEventCalled_Flag = true;
         }
+
+
     }
 }
