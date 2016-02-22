3e563daad326b2be81f817939cc08e1a7307f6c4
Igor Kotlerman <igor.kotlerman@gmail.com>
2016-02-22 17:39:44 +0200

    Added core classes for user dictionaries and masks
    - Added base class CrackDataSource
    - Added Dictionary class
    - Added Mask class

---
+++ LeukocyteGUI_for_oclHashCat/CrackDataSource.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeukocyteGUI_for_oclHashCat
+{
+    public class CrackDataSource
+    {
+        string name = "";
+        string description = "";
+        string source = "";
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        public string Source
+        {
+            get
+            {
+                return source;
+            }
+            set
+            {
+                source = value;
+            }
+        }
+    }
+}
